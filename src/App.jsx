1bc09738c24b0cce775f5cf5549f08bf526820f0
Tyree Webber <webber.tyree@gmail.com>
2022-08-19 08:44:54 -0600

    import components into main app and declare a few global css variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import Projects from './components/projects/Projects'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+        <Header />
+        <Nav />
+        <About />
+        <Projects />
+        <Contact />
+        <Footer />
+    </> 
   )
 }
 
