76b8987c4183e2a1d2a7d69dfb64900a32e62753
Tyree Webber <webber.tyree@gmail.com>
2022-08-22 16:15:19 -0600

    create CTA and Socials sub-components and work on header

---
+++ src/components/header/Socials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Socials = () => {
+  return (
+    <div className='header__socials'>
+      <a href="https://www.linkedin.com/in/tyree-webber-b0815a14b/" target="_blank"></a>
+      <a href="https://github.com/TyreeWebber" target="_blank"></a>
+    </div>
+  )
+}
+
+export default Socials
