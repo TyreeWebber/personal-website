d00d1ae09bcdd999388b8a62195f63dd5b46f664
Tyree Webber <webber.tyree@gmail.com>
2022-08-25 16:29:35 -0600

    create projects component and begin styling. (may implement carousel in the future)

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
 import './projects.css'
+import IMG1 from '../../assets/scheduler.png'
+import IMG2 from '../../assets/happyeats.png'
+import IMG3 from '../../assets/dinobattleroyale.png'
 
 const Projects = () => {
   return (
-    <section id='projects'>Projects</section>
+    <section id='projects'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src= {IMG1} alt="" />
+          </div>
+          <h3>Scheduler</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com/TyreeWebber/scheduler" className='btn btn-primary' target='_blank'>GitHub</a>
+          </div>
+        </article>
+      </div>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src= {IMG2} alt="" />
+          </div>
+          <h3>Happy Eats</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com/TyreeWebber/food-delivery-app" className='btn btn-primary' target='_blank'>GitHub</a>
+          </div>
+        </article>
+      </div>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src= {IMG3} alt="" />
+          </div>
+          <h3>Dino: Battle Royale</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com/TyreeWebber/lhl-final" className='btn btn-primary' target='_blank'>GitHub</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
