b91348dd67a9191a607b63a735f9785e572638f4
Tyree Webber <webber.tyree@gmail.com>
2022-09-01 14:27:43 -0600

    create jsx for footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {RiInstagramFill} from 'react-icons/ri'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Tyree Webber</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/tyreewebber/"><BsLinkedin/></a>
+        <a href="https://www.instagram.com/tyreewebber_/"><RiInstagramFill/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; 2022 Tyree Webber. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
